da09e283feafdb88a161de004e76aac0d4c5e3a5
Henny Yoffe <hennyy1149@gmail.com>
2019-04-14 14:59:05 -0400

    Add project files.

---
+++ HW58_Ecommerce_April14/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW58_Ecommerce_April14.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
